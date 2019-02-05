5cb538b7bd0d4f82489adda177e08cf8c34f1def
github username <github email address>
2019-02-04 19:13:13 -0500

    tetris game is complete just need to add fix for rotate

--- src/Components/TetrisCanvas.jsx
+++ src/Components/TetrisCanvas.jsx
@@ -2,19 +2,30 @@ import React, { Component } from 'react';
 import p5 from 'p5'
 import {createPeace} from '../shapes'
 import {connect} from 'react-redux'
+import {matrixBag} from '../shapesAgain'
 
 
-const shapes = ["I","T","O","L","J","Z","S"]
 
+
+const createMatrix=(w,h)=> {
+  const matrix = []
+  while (h--){
+    matrix.push(new Array(w).fill(0))
+  }
+  return matrix
+}
 
 class TetrisCanvas extends Component {
 
   state ={
+    score: 0,
+    arena: createMatrix(20, 35),
     matrix: [
       [0,0,0],
       [1,1,1],
       [0,1,0],],
-    pos: {x:8,y:0}
+    pos: {x:8,y:0},
+    test: null
   }
 
 
@@ -26,15 +37,26 @@ class TetrisCanvas extends Component {
     return matrix
   }
 
-  checkCollision = (arena)=> {
+  updateScore =()=> {
+    this.setState({})
+  }
+
+  checkCollision = ()=> {
+    console.log("laughing at arena", this.state.arena);
     for (let y = 0; y < this.state.matrix.length; ++y){
       for (let x = 0; x < this.state.matrix[y].length; ++x){
-        if(this.state.matrix[y][x] !== 0 && (arena[y + this.state.pos.y] && arena[y + this.state.pos.y][x+this.state.pos.x]) !== 0){
+
+        if(this.state.matrix[y][x] !== 0 && (this.state.arena[y + this.state.pos.y] && this.state.arena[y + this.state.pos.y][x+this.state.pos.x]) !== 0){
           return true
+          // this.setState({
+          //   test: true,
+          //   pos: {...this.state.pos, y: this.state.pos.y-1}
+          // },(arena)=> this.mergeBlocks(arena))
         }
       }
     }
     return false
+    console.log("heywassup");
   }
 
   mergeBlocks =(arena)=> {
@@ -45,44 +67,138 @@ class TetrisCanvas extends Component {
         }
       })
     })
+    // this.setState({pos: {...this.state.pos, y: 0}})
+  }
+
+  checkReset=()=>{
+    if(this.checkCollision(this.state.arena)=== true && this.state.pos.y > -1){
+      window.alert("ha ha uyou lose bet you didnt see that coming ")
+      let arena = this.createMatrix(20,35)
+      console.log("WAIT YOU NEED ME TO TEST ", this.state.arena.forEach(row => row.fill(0)))
+      this.setState({
+        arena: arena,
+        score: 0
+      })
+    }
+  }
+
+  newDrop=()=>{
+    const shapes = ["I","T","O","L","J","Z","S"]
+    let randomShapes = shapes[Math.floor(Math.random() * shapes.length)]
+    let piece = createPeace(randomShapes)
+    console.log("checkPIECE",piece,randomShapes);
+    this.mergeBlocks(this.state.arena)
+    this.arenaClear()
+    this.setState({
+      matrix: piece,
+      pos: {...this.state.pos, y: 1, x: 8}
+    },() => this.checkReset(piece))
   }
 
   draw=()=> {
     let ctx = this.refs.canvas.getContext('2d');
+    let arena = this.state.arena
     ctx.fillStyle ="#000";
     ctx.fillRect(0,0,500,700)
-    this.drawMatrix(this.state.matrix,this.state.pos,ctx)
+    if(this.checkCollision(arena) === false){
+      this.drawMatrix(this.state.matrix,this.state.pos,ctx)
+    } else if (this.checkCollision(arena) === true){
+      this.setState({
+        pos: {...this.state.pos, y: this.state.pos.y-1}
+      },() => this.newDrop() )
+    }
+    this.drawMatrix(arena,{x:0,y:0},ctx)
+  }
+  // this.drawMatrix(this.state.matrix,this.state.pos,ctx)
+
+  playerRotate=()=>{
+    if (this.checkCollision(this.state.arena)=== true){
+      this.rotateMatrix()
+    }
   }
 
+  rotateMatrix = () => {
+    for (let y = 0; y < this.state.matrix.length; ++y){
+      for (let x = 0; x < y; ++x){
+        [
+          this.state.matrix[x][y],
+          this.state.matrix[y][x],
+        ] = [
+          this.state.matrix[y][x],
+          this.state.matrix[x][y],
+        ]
+      }
+    }
+      this.setState({
+        matrix: this.state.matrix.reverse()
+      },()=> this.draw())
+  }
 
-  updateCanvas=(ctx)=> {
+  arenaClear= ()=> {
+    let rowCount = 1
+    outer: for (let y = this.state.arena.length -1; y > 0; --y){
+      for(let x = 0; x < this.state.arena[y].length; ++x){
+        if(this.state.arena[y][x] === 0){
+          continue outer
+        }
+      }
+      const row = this.state.arena.splice(y,1)[0].fill(0)
+        this.state.arena.unshift(row)
+        ++y
+
+        this.setState({
+          score: this.state.score += rowCount * 10
+        })
+    }
+  }
+
+  updateCanvas=(ctx,arena)=> {
         // let ctx = this.refs.canvas.getContext('2d');
         ctx.fillRect(20, 20, 500, 700)
         ctx.fillStyle =
         ctx.scale(20,20)
-        this.update(ctx)
+        // this.update(ctx,arena)
       }
 
-    update=(ctx)=>{
-        this.draw(ctx)
-
-       setInterval(()=> this.setState({
+    dropMatrix=(ctx)=>{
+      setInterval(()=> this.setState({
         pos: {...this.state.pos, y: this.state.pos.y+1}
       },() => {
-        this.draw(ctx)
-      }),1000)
+        this.draw()
+      }),500)
+    }
 
+    update=(ctx)=>{
+      console.log("this arena");
+
+          this.draw()
+          this.dropMatrix(ctx)
 
     }
 
+    playerMove(dir){
+      this.setState({pos: {...this.state.pos, x: this.state.pos.x+=dir}},()=> this.draw())
+      if (this.checkCollision(this.state.arena)=== true){
+        this.setState({pos: {...this.state.pos, x: this.state.pos.x-=dir}},()=> this.draw())
+      }
+    }
+
       drawMatrix=(matrix,offset,ctx)=> {
-        // let ctx = this.refs.canvas.getContext('2d');
+        const colors =[
+          null,
+          '#FF0000',
+          '#00BFFF',
+          '#00FF00',
+          '#FF00FF',
+          '#DAA520',
+          '#FF8C00',
+          '#1E90FF',
+          '#F5F5F5'
+        ]
         matrix.forEach((row,y) => {
           row.forEach((value,x)=> {
             if (value !== 0) {
-              // ctx.createPattern(matrix,"repeat")
-              // ctx.clearRect(20,20,500,700)
-              ctx.fillStyle ="blue"
+              ctx.fillStyle =colors[value]
               ctx.fillRect(x+ offset.x,y+ offset.y,1,1)
 
             }
@@ -90,86 +206,55 @@ class TetrisCanvas extends Component {
         })
       }
 
-  componentWillUpdate(){
-    const canvas = this.refs.canvas
-    const ctx = canvas.getContext("2d")
-
-
-    }
-
 
 
   componentDidMount(){
     const canvas = this.refs.canvas
     const ctx = canvas.getContext("2d")
+    const arena = this.createMatrix(20,35)
+
+    this.setState({arena},()=> console.log("looking at this", arena))
+
     canvas.addEventListener("keydown",(e)=> console.log(e))
     // ctx.clearRect(0,0,20,20)
     this.updateCanvas(ctx)
-
     // this.draw(ctx)
     this.update(ctx)
-    const arena = this.createMatrix(12,20)
+    // debugger
 
-    this.checkCollision(arena)
+    // console.log("chicken",this.checkCollision(arena));
 
-    console.log(arena);
-    console.table(arena)
-    // this.sketch = new p5(p => {
-    //   console.log(p);
-    //
-    //
-    //   p.setup = () => {
-    //     p.createCanvas(400, 500).parent(this.renderRef.current)
-    //   }
-    //
-    //
-    //   p.draw = () => {
-    //     let piece = createPeace("T")
-    //     piece.forEach((row,y) => {
-    //       row.forEach((value,x)=> {
-    //         if (value !== 0) {
-    //           p.background(0)
-    //           p.fill("red")
+    // console.log(arena);
+    // console.table(arena)
 
-              // p.piece(this.state.x,this.state.y)
-        //     }
-        //   })
-        // })
-        // console.log(piece)
-        // p.background(0, 25,25)
-        // p.fill('red')
-        // p.circle(this.state.x,this.state.y,50,50)
-    //   }
-    //
-    //   setInterval(()=> this.setState({
-    //     y: this.state.y + 1
-    //   }),10)
-    //
-    //
-    // })
 
   }
 
 onKeyPressed =(e,ctx)=> {
   console.log(e);
-  if (e.keyCode === 37 && this.state.pos.x-1 >= 0){
-    this.setState({pos: {...this.state.pos, x: this.state.pos.x-1}},()=> this.draw())
+  if (e.keyCode === 37){
+    this.playerMove(-1)
+    // this.setState({pos: {...this.state.pos, x: this.state.pos.x-1}},()=> this.draw())
   } else if (e.keyCode === 39){
-    this.setState({pos: {...this.state.pos, x: this.state.pos.x+1}},()=> this.draw())
+    this.playerMove(+1)
+    // this.setState({pos: {...this.state.pos, x: this.state.pos.x+1}},()=> this.draw())
   } else if (e.keyCode === 40){
     this.setState({pos: {...this.state.pos, y: this.state.pos.y+1}},()=> this.draw())
+  } else if (e.keyCode === 38) {
+    this.rotateMatrix()
   }
 }
 
 
   render() {
-    console.log(this.state);
+    console.log(this.state, "render");
     this.renderRef = React.createRef()
 
     return (
-      <div
 
+      <div
         >
+        Your Total Score{this.state.score}
        <canvas
         tabindex='1'
         onKeyDown={(e) => this.onKeyPressed(e)}
